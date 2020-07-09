f65659313b237293649adfdf19cb3154dbec9524
Anthony Kawa <anthonyk2020@gmail.com>
2020-07-08 19:25:14 -0700

    parsing user input, and added overwrote string method for room class

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,6 @@
 from room import Room
+from player import Player
+import textwrap
 
 # Declare all the rooms
 
@@ -33,19 +35,55 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+# Parses moves
+def parse_move(room, move):
+    valid_moves = ["n", "s", "e", "w"]
+
+    if(move == "q"):
+        return
+
+    try:
+        valid_moves.index(move)
+    except:
+        print("Not a valid move. Try again")
+        return room
+
+    try:
+        if(move == "n"):
+            return room.n_to
+        elif(move == "s"):
+            return room.s_to
+        elif(move == "w"):
+            return room.w_to
+        elif(move == "e"):
+            return room.e_to
+    except:
+        print("No room there. Try again.")
+        return room
+
 #
 # Main
 #
+is_playing = True
+while(is_playing):
 
-# Make a new player object that is currently in the 'outside' room.
-
-# Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
-# If the user enters a cardinal direction, attempt to move to the room there.
-# Print an error message if the movement isn't allowed.
-#
-# If the user enters "q", quit the game.
+    # Make a new player object that is currently in the 'outside' room.
+    name = input("What do you want to be called? ")
+    player = Player(name, room['outside'])
+    # Write a loop that:
+    #
+    has_won = False
+    while(not has_won and is_playing):
+        # * Prints the current room name
+        # * Prints the current description (the textwrap module might be useful here).
+        print(player.current_room)
+        # * Waits for user input and decides what to do.
+        move = input("What would you like to do? ")
+        if(move == "q"):
+            is_playing = False
+        player.current_room = parse_move(player.current_room, move)
+        #
+        # If the user enters a cardinal direction, attempt to move to the room there.
+        # Print an error message if the movement isn't allowed.
+        #
+        # If the user enters "q", quit the game.
--- src/room.py
+++ src/room.py
@@ -1,2 +1,13 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+import textwrap
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return (f"""\n{'='*60}\nCurrent Location: {self.name}\n\n{textwrap.fill(self.description, width=60)}\n{'='*60}\n""")
+
